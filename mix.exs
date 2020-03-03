26a5bb452a4b7bfd71853e5de3f52c536f04cbce
Rémi Prévost <rprevost@mirego.com>
2020-03-03 14:52:20 -0500

    Improve mix.exs

--- mix.exs
+++ mix.exs
@@ -1,12 +1,32 @@
 defmodule MixAudit.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [
       app: :mix_audit,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      docs: [
+        extras: ["README.md"],
+        main: "readme",
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/mirego/mix_audit"
+      ],
+      description:
+        "MixAudit provides a `mix deps.audit` task to scan Mix dependencies for security vulnerabilities.",
+      source_url: "https://github.com/mirego/mix_audit",
+      homepage_url: "https://github.com/mirego/mix_audit",
+      docs: [
+        extras: ["README.md"],
+        main: "readme",
+        source_ref: "v#{@version}",
+        source_url: "https://github.com/mirego/mix_audit"
+      ],
+      package: package(),
+      start_permanent: false,
       deps: deps()
     ]
   end
@@ -22,5 +42,15 @@ defmodule MixAudit.MixProject do
       {:ecto, "2.2.0"},
       {:yaml_elixir, "~> 2.4.0"}
     ]
+  end
+
+  defp package do
+    %{
+      maintainers: ["Rémi Prévost"],
+      licenses: ["BSD-3"],
+      links: %{
+        "GitHub" => "https://github.com/mirego/mix_audit"
+      }
+    }
   end
 end
