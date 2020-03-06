e1e6585ae9647537b4c4d4fd9c5732acab19413d
Rémi Prévost <remi@exomel.com>
2020-03-06 14:59:34 -0500

    Update --help text

--- lib/mix_audit/cli/help.ex
+++ lib/mix_audit/cli/help.ex
@@ -5,8 +5,10 @@ defmodule MixAudit.CLI.Help do
     IO.puts("Example: $ mix deps.audit --path=/home/projects/my_app --format=json")
     IO.puts("")
     IO.puts("Options:")
-    IO.puts("--path       The root path of the project to audit")
-    IO.puts("--format     The format of the report to generate (human, json)")
+    IO.puts("--path                  The root path of the project to audit")
+    IO.puts("--format                The format of the report to generate (human, json)")
+    IO.puts("--ignore-advisory-ids   A comma-separated list of advisory IDs to ignore")
+    IO.puts("--ignore-package-names  A comma-separated list of package names to ignore")
     IO.puts("")
     System.halt(0)
   end
