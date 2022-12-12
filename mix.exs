21bc5ee092eb86612c16524a9edd6339dd5b3663
Christoph Grothaus <me@cgrothaus.de>
2022-12-12 15:52:17 +0100

    Update `yaml_elixir` dependency constraint to `~> 2.9` to get the latest version

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule MixAudit.MixProject do
 
   defp deps do
     [
-      {:yaml_elixir, "~> 2.8.0"},
+      {:yaml_elixir, "~> 2.9"},
       {:jason, "~> 1.1"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:credo_naming, "~> 0.4", only: [:dev, :test], runtime: false}
